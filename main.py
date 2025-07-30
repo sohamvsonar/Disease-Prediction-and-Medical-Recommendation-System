03e151e74056af18f266948021f94c5d5d840553
Soham Sonar <soham.sonar427@gmail.com>
2025-07-30 00:38:02 -0500

    Add deployment files

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle
 from fuzzywuzzy import process
 import ast
+import os
 
 app = Flask(__name__)
 
@@ -120,5 +121,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
